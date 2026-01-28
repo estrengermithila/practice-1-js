54d0ac18f4ef5bf718ac4ca007bcc859bb66b8fb
estrengermithila <mithilafarjanam20@gmail.com>
2026-01-29 00:59:23 +0600

    commited

--- first.js
+++ first.js
@@ -1690,20 +1690,22 @@
 // console.log(checkPositiveNumber("5"));  // Invalid Input
 // console.log(checkPositiveNumber(0));
 
-function validateProduct(product){
-    if(typeof product !== "object" || product === null || Array.isArray(product)){
-        return "Invalid Input"
-    }
-    const {name,price,quantity} = product
-    if(typeof name !== "string" || typeof price !== "number" || typeof quantity !== "number" || price<0 || quantity<0){
-        return "Invalid Input"
-    }
-    else if(typeof name === "string" || price>=0 || quantity>=0){
-        return "Valid Product"
-    }
-}
+// function validateProduct(product){
+//     if(typeof product !== "object" || product === null || Array.isArray(product)){
+//         return "Invalid Input"
+//     }
+//     const {name,price,quantity} = product
+//     if(typeof name !== "string" || typeof price !== "number" || typeof quantity !== "number" || price<0 || quantity<0){
+//         return "Invalid Input"
+//     }
+//     else if(typeof name === "string" || price>=0 || quantity>=0){
+//         return "Valid Product"
+//     }
+// }
 
-console.log(validateProduct({ name: "Laptop", price: 1000, quantity: 5 })); // Valid Product
-console.log(validateProduct({ name: "Laptop", price: -100, quantity: 5 }));  // Invalid Input
-console.log(validateProduct(["Laptop", 1000, 5]));                           // Invalid Input
-console.log(validateProduct(null)); 
+// console.log(validateProduct({ name: "Laptop", price: 1000, quantity: 5 })); // Valid Product
+// console.log(validateProduct({ name: "Laptop", price: -100, quantity: 5 }));  // Invalid Input
+// console.log(validateProduct(["Laptop", 1000, 5]));                           // Invalid Input
+// console.log(validateProduct(null)); 
+
+//dom
