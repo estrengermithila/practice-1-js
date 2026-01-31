445f89ed0122c810204825d8f53ce299fcc1f542
estrengermithila <mithilafarjanam20@gmail.com>
2026-01-31 23:16:58 +0600

    commited

--- first.js
+++ first.js
@@ -1712,7 +1712,11 @@ let header = document.getElementById("heading");
 console.log(header)
 
 let quSl = document.querySelector("div")
-console.log(quSl.innerText)
+// console.log(quSl.innerText)
+// console.log(validateProduct({ name: "Laptop", price: 1000, quantity: 5 })); // Valid Product
+// console.log(validateProduct({ name: "Laptop", price: -100, quantity: 5 }));  // Invalid Input
+// console.log(validateProduct(["Laptop", 1000, 5]));                           // Invalid Input
+// console.log(validateProduct(null)); 
 console.log(validateProduct({ name: "Laptop", price: 1000, quantity: 5 })); // Valid Product
 console.log(validateProduct({ name: "Laptop", price: -100, quantity: 5 }));  // Invalid Input
 console.log(validateProduct(["Laptop", 1000, 5]));                           // Invalid Input
