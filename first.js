783e2a1761a2286044ec3d1a4e8976a7cd313dbd
estrengermithila <mithilafarjanam20@gmail.com>
2026-02-01 23:56:37 +0600

    commited

--- first.js
+++ first.js
@@ -1752,3 +1752,13 @@ console.log(validateProduct(null));
 // console.log(validateProduct({ name: "Laptop", price: -100, quantity: 5 }));  // Invalid Input
 // console.log(validateProduct(["Laptop", 1000, 5]));                           // Invalid Input
 // console.log(validateProduct(null)); 
+
+
+// console.log(validateProduct({ name: "Laptop", price: 1000, quantity: 5 })); // Valid Product
+// console.log(validateProduct({ name: "Laptop", price: -100, quantity: 5 }));  // Invalid Input
+// console.log(validateProduct(["Laptop", 1000, 5]));                           // Invalid Input
+// console.log(validateProduct(null)); 
+// console.log(validateProduct({ name: "Laptop", price: 1000, quantity: 5 })); // Valid Product
+// console.log(validateProduct({ name: "Laptop", price: -100, quantity: 5 }));  // Invalid Input
+// console.log(validateProduct(["Laptop", 1000, 5]));                           // Invalid Input
+// console.log(validateProduct(null)); 
